9982185816cedb099c4ae9ebb29e5f6bc6f79678
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-01 15:06:06 -0400

    form setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,37 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import data from './data';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  };
+
+  return (
+    <section className='section-center'>
+      <h4>tired of boring lorem?</h4>
+      <form className='lorem-form'>
+        {/* <h4>Add User</h4> */}
+        <label htmlFor='amount'>Paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          min={1}
+          step={1}
+          max={8}
+          value={count}
+          onChange={handleChange}
+        />
+        <button className='btn' type='submit'>
+          generate
+        </button>
+      </form>
+      {/* render users below */}
+    </section>
+  );
 };
 export default App;
